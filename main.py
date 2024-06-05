a61bf22d03b29de98ce18f60c0f3d6f59f493706
Natalia Bazhina <nataliaagapova27@yandex.ru>
2024-06-05 13:05:55 +0300

    add test_secure

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from src.functions import get_operations, sort_data, date_prettify
+from src.functions import get_operations, sort_data, date_prettify, secure
 
 
 def main():
     operations = get_operations('operations.json')
     operations = sort_data(operations)
-    for element in operations:
-        description = element["description"]
-        date_new = element['date']
-        sender = element['from']
-        reciever = element['to']
-        amount = element['operationAmount']['amount']
-        currency = element['operationAmount']['currency']['name']
-        print(date_prettify(date_new), description)
-        print(sender, '->', reciever)
-        print(amount, currency, end='\n\n')
+    # for element in operations:
+    #     description = element["description"]
+    #     date_new = element['date']
+    #     sender = secure(element['from'])
+    #     reciever = secure(element['to'])
+    #     amount = element['operationAmount']['amount']
+    #     currency = element['operationAmount']['currency']['name']
+    #     print(date_prettify(date_new), description)
+    #     print(sender, '->', reciever)
+    #     print(amount, currency, end='\n\n')
+    print(operations)
 
 if __name__=='__main__':
     main()
--- src/functions.py
+++ src/functions.py
@@ -32,3 +32,20 @@ def date_prettify(date: str) -> str:
     date_new = date[0:10].split('-')
     date_new = ".".join(ch for ch in reversed(date_new))
     return date_new
+
+
+def secure(payment_number: str) -> str:
+    """
+    Получает информацию откуда и куда совершен перевод
+    Вывод преобразованную информацию в формате:
+    номер карты XXXX XX** **** XXXX; номер счета **XXXX
+    """
+    from_data_split = payment_number.split(' ')
+    num = from_data_split[-1]
+    if from_data_split[0] == 'Счет':
+        return f'{from_data_split[0]} **{num[-4:]}'
+
+    if from_data_split[0] == 'Visa':
+        return f'{from_data_split[0]} {from_data_split[1]} {num[0:4]} {num[4:6]}** **** {num[-4:]}'
+
+    return f'{from_data_split[0]} {num[0:4]} {num[4:6]}** **** {num[-4:]}'
